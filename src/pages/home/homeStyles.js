ec9b8e17fe3d439da9bea2e68dab31b6a19a512b
felipegith <felipefknowx@gmail.com>
2023-01-17 22:07:09 -0300

    Versionamento do convite

---
+++ src/pages/home/homeStyles.js
@@ -0,0 +1,373 @@
+import styled from "styled-components";
+import background from "../../assets/images/fundo.jpg";
+import { device } from "../../utils/Responsive";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100%;
+
+    background-image: url(${background});
+    //background-repeat: no-repeat;
+`;
+
+export const ContainerRight = styled.div`
+    width: 100%;
+
+    display: flex;
+`;
+
+export const Right = styled.div`
+    width: 100%;
+    background-color: red;
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+`;
+
+export const Left = styled.div`
+    width: 50%;
+`;
+export const ImageRaim = styled.img`
+    transform: rotate(50deg);
+`;
+
+export const Main = styled.main`
+    /* margin-bottom: 50%; */
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    @media ${device.mobileS} {
+        text-align: center;
+    }
+`;
+export const Title = styled.h1`
+    //font-family: "Herr Von Muellerhoff", cursive;
+    font-family: "Monsieur La Doulaise", cursive;
+    font-size: 3.5rem;
+    font-weight: 100;
+
+    color: #394d2e;
+`;
+
+export const TextBorderRadiusMons = styled.span`
+    font-family: "Corben", cursive;
+    font-style: italic;
+    font-size: 10rem;
+    font-weight: 500;
+
+    color: #fff;
+    line-height: 90%;
+`;
+export const ContainerContent = styled.div`
+    padding: 10%;
+`;
+
+export const Content = styled.h1`
+    font-family: "Corben", cursive;
+    text-align: center;
+    font-weight: 200;
+    font-size: 1rem;
+    color: #394d2e;
+`;
+
+export const ContainerTopRadius = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const ContentContainerRadius = styled.div`
+    width: 70%;
+    height: 70vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background-color: #bacea5;
+    border-top-left-radius: 250px;
+    border-top-right-radius: 250px;
+`;
+
+export const TextBorderRadius = styled.span`
+    font-family: "Corben", cursive;
+    font-size: 5rem;
+    font-weight: 500;
+
+    color: #fff;
+    line-height: 80%;
+`;
+
+export const ContainerTextAndLocation = styled.div`
+    margin-top: 2%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
+export const TextLocation = styled.span`
+    font-size: 1rem;
+    font-family: "Corben", cursive;
+    line-height: 100%;
+
+    color: #394d2e;
+`;
+
+export const ContainerCircleLocation = styled.div`
+    width: 80%;
+    max-width: 95%;
+    background-color: #bacea5;
+    padding: 10px;
+    border-radius: 20px;
+    margin-top: 4%;
+    cursor: pointer;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const TextLinkLocation = styled.a`
+    font-family: "Corben", cursive;
+    text-decoration: none;
+    color: #222a21;
+    font-size: 0.9rem;
+    text-align: center;
+`;
+
+export const ContainerGreenDescription = styled.div`
+    width: 100%;
+    padding: 15px;
+    margin-top: 7%;
+    display: flex;
+    flex-direction: column;
+    background-color: #bacea5;
+`;
+
+export const TitleDescription = styled.span`
+    font-size: 1rem;
+    font-family: "Corben", cursive;
+    color: #394d2e;
+`;
+
+export const TextContentDescription = styled.span`
+    font-size: 0.9rem;
+    font-weight: 500;
+    font-family: "Corben", cursive;
+    font-style: italic;
+    text-align: justify;
+    margin-left: 1%;
+    color: #394d2e;
+`;
+
+export const ContainerGreenFooterCenter = styled.div`
+    width: 100%;
+    margin-top: 2%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const TextFooterGreen = styled.span`
+    text-align: center;
+    font-family: "Corben", cursive;
+    font-style: italic;
+    font-size: 0.8rem;
+    color: #394d2e;
+`;
+
+export const ContainerCenterValues = styled.div`
+    width: 100%;
+    margin-top: 22%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const ContainerCenterFlex = styled.div`
+    width: 70%;
+    margin-top: 2%;
+    padding: 1%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    background-color: #bacea5;
+`;
+export const TitleValueText = styled.span`
+    font-family: "Corben", cursive;
+    font-size: 1rem;
+`;
+
+export const TextContentValue = styled.span`
+    font-family: "Corben", cursive;
+    text-align: center;
+    font-size: 0.7rem;
+`;
+
+export const ContainerCenterValuesColumn = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const ContainerCenterFlexRow = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const TextContentValueWidth = styled.span`
+    font-family: "Corben", cursive;
+
+    text-align: justify;
+    font-size: 0.7rem;
+    width: 50%;
+`;
+
+export const TextContentValueRight = styled.span`
+    font-family: "Corben", cursive;
+    text-align: center;
+    font-size: 0.7rem;
+    width: 50%;
+`;
+
+export const TextFooterCenterValues = styled.span`
+    font-family: "Corben", cursive;
+    text-align: center;
+    font-size: 0.7rem;
+    font-style: italic;
+    margin-top: 2%;
+`;
+
+export const ContainerSocials = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin-top: 10%;
+    padding-left: 10%;
+    padding-right: 10%;
+`;
+
+export const TextSocials = styled.span`
+    font-family: "Corben", cursive;
+    font-style: italic;
+    text-align: center;
+    color: #394d2e;
+`;
+
+export const ContainerSocialsCircleRow = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+`;
+
+export const ContainercircleSocial = styled.a`
+    width: 80px;
+    height: 80px;
+    border-radius: 40px;
+    background-color: #bacea5;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const LogoSocials = styled.img`
+    width: 100%;
+`;
+
+export const TextCelebration = styled.span`
+    font-family: "Corben", cursive;
+    font-style: italic;
+    text-align: center;
+    color: #394d2e;
+`;
+
+export const ContainerCelebration = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
+
+export const ContainerCelebrationLine = styled.div`
+    width: 50%;
+    margin-top: 15%;
+`;
+
+export const ContainerFooterInvite = styled.div`
+    width: 100%;
+    margin-top: 5%;
+    padding-left: 5%;
+    padding-right: 5%;
+    background-color: #bacea5;
+`;
+
+export const ContainerFooterInviteTitle = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
+
+export const TextFooterInvite = styled.span`
+    font-family: "Corben", cursive;
+    font-style: italic;
+    text-align: center;
+    color: #394d2e;
+`;
+
+export const ContainerButtonInvite = styled.div`
+    background-color: #eadfc1;
+
+    width: 70%;
+    max-width: 95%;
+    padding: 10px;
+    border-radius: 20px;
+    margin-top: 4%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const ContainerFooterResume = styled.div`
+    margin-top: 5%;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const TextResumeTitle = styled.span`
+    font-family: "Corben", cursive;
+    color: #394d2e;
+`;
+
+export const ContenxtResume = styled.span`
+    font-family: "Corben", cursive;
+    font-style: italic;
+    text-align: justify;
+    font-size: 0.9rem;
+    color: #394d2e;
+`;
+
+export const Footer = styled.div`
+    margin-top: 10%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const TextLastFooter = styled.span`
+    font-family: "Corben", cursive;
+    text-align: center;
+    font-size: 0.9rem;
+    color: #394d2e;
+`;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,211 @@
+import React from 'react'
+
+import Ramin from '../../assets/images/raminho.png'
+import Facebook from '../../assets/images/FB.png'
+import Instagram from '../../assets/images/IG.png'
+import Site from '../../assets/images/www.png'
+
+import { Container, ContainerRight, Right, Left, ImageRaim, Main, Title, TextLocation,
+        TextBorderRadius, Content, ContainerContent, ContainerTextAndLocation, ContentContainerRadius,
+        ContainerTopRadius, ContainerCircleLocation, ContainerGreenDescription, TextLinkLocation, TitleDescription,
+        TextContentDescription, ContainerGreenFooterCenter, TextFooterGreen, ContainerCenterValues, TitleValueText, TextContentValue,
+        ContainerCenterFlex, TextBorderRadiusMons, ContainerCenterFlexRow, ContainerCenterValuesColumn, TextContentValueWidth, TextContentValueRight,
+        TextFooterCenterValues, ContainerSocials, TextSocials, ContainerSocialsCircleRow, ContainercircleSocial, LogoSocials, ContainerCelebration, TextCelebration,
+        ContainerCelebrationLine, ContainerFooterInvite, ContainerFooterInviteTitle, TextFooterInvite, ContainerButtonInvite, ContainerFooterResume, TextResumeTitle,
+        ContenxtResume, Footer, TextLastFooter
+    } from './homeStyles'
+
+export default function Home(){
+    return(
+        <Container>
+            {/* <ContainerRight>
+                <Left>
+                    <p>.</p>
+                </Left>
+                <Right>
+                    <ImageRaim src={Ramin} />
+                </Right>
+            </ContainerRight>  */}
+            <br></br>
+            <br></br>
+            <br></br>
+                        
+            <Main>
+                <Title>
+                    Maiara Fernandes
+                </Title>
+                <Title>
+                    &
+                </Title>
+                <Title>
+                    Felipe Costa
+                </Title>
+            </Main>
+            
+            <ContainerContent>
+                <Content>
+                ESTAMOS MUITO FELIZES EM PODER
+                COMPARTILHAR COM VOCÊS A ALEGRIA DE MAIS
+                UM PASSO DADO EM NOSSAS VIDAS: O NOSSO
+                CASAMENTO!
+                </Content>
+            </ContainerContent>
+            <ContainerTopRadius>
+                <ContentContainerRadius>
+                    <TextBorderRadiusMons>14</TextBorderRadiusMons>
+                    <TextBorderRadius>JUN</TextBorderRadius>
+                    <TextBorderRadius>2023</TextBorderRadius>
+                    <TextBorderRadiusMons>19h</TextBorderRadiusMons>
+                </ContentContainerRadius>
+            </ContainerTopRadius>
+            <ContainerTextAndLocation>
+                <TextLocation>
+                    NA CHURRASCARIA
+                </TextLocation>
+                <TextLocation>
+                TOURÃO
+                </TextLocation>
+                <ContainerCircleLocation>
+                    <TextLinkLocation href="https://www.google.com/maps/place/Tour%C3%A3o/@-23.0114991,-43.3201257,17z/data=!3m1!4b1!4m5!3m4!1s0x9bd097ceaaaaab:0xa74eebcc851823e4!8m2!3d-23.0115769!4d-43.3179581" target="_blank">
+                    Clique aqui para vizualizar a localização no mapa
+                    </TextLinkLocation>
+                </ContainerCircleLocation>
+            </ContainerTextAndLocation>
+            <ContainerGreenDescription>
+                <TitleDescription>
+                    Às nossas queridas amizades e família,
+                    <br></br>
+                    <br></br>
+                </TitleDescription>
+                <TextContentDescription>
+                    Sabemos que é tempo de festejar e comemorar um passo tão importante tendo em vista
+                 que foi necessário superar muitos obstáculos, provamos a nós mesmos que o amor contribuiu 
+                 para as nossas conquistas e que sem o apoio de cada um que convidamos não estaríamos aqui.
+                    <br></br>
+                    <br></br>
+                </TextContentDescription>
+
+                <TextContentDescription>
+                    Nesse período de 4 anos, enfrentamos uma pandemia, as dificuldades da vida,
+                    a perda de pessoas queridas mas também conquistamos o fechamento de novos ciclos acadêmicos,
+                    profissionais e pessoais e principalmente o amor um pelo outro e o carinho de cada um dos nossos
+                    convidados para esse jantar.
+                    <br></br>
+                    <br></br>
+                </TextContentDescription>
+                <TextContentDescription>
+                Nesse passo tão incrível e mágico, celebraremos com um jantar de casamento será em formato
+                de jantar por adesão* com mais informações abaixo.
+                <br></br>
+                <br></br>
+                </TextContentDescription>
+                <ContainerGreenFooterCenter>
+                    <TextFooterGreen>
+                        * O formato jantar por adesão significa que o consumo é por conta do Convidado.
+                    </TextFooterGreen>
+                </ContainerGreenFooterCenter>
+            </ContainerGreenDescription>
+
+            <ContainerCenterValues>
+                <ContainerCenterFlex>
+                <TitleValueText>
+                    VALORES
+                </TitleValueText>
+                <TextContentValue>
+                RODÍZIO DE CARNES + BUFFET COMPLETO COM GUARNIÇÕES, SALADAS E PRATOS QUENTES
+                <br></br>
+                <br></br>
+                </TextContentValue>
+                <ContainerCenterValuesColumn>
+                <ContainerCenterFlexRow>
+                <TextContentValueWidth>
+                    ADULTOS
+                </TextContentValueWidth>
+                
+                <TextContentValueRight>
+                    98,
+                </TextContentValueRight>
+                </ContainerCenterFlexRow>
+
+                <ContainerCenterFlexRow>
+                <TextContentValueWidth>
+                CRIANÇAS ATÉ 5 ANOS
+                </TextContentValueWidth>
+                <TextContentValueRight>
+                    00,
+                </TextContentValueRight>
+                </ContainerCenterFlexRow>
+
+                <ContainerCenterFlexRow>
+                <TextContentValueWidth>
+                    MAIORES DE 18 ANOS
+                </TextContentValueWidth>
+                <TextContentValueRight>
+                    49,
+                </TextContentValueRight>
+                </ContainerCenterFlexRow>
+                </ContainerCenterValuesColumn>
+                <TextFooterCenterValues>
+                ** não inclui bebidas e taxas de serviço
+                </TextFooterCenterValues>
+                </ContainerCenterFlex>
+                </ContainerCenterValues>
+                <ContainerSocials>
+                    <TextSocials>Para mais informações sobre o que será servido, é possível conferir nas redes do restaurante:</TextSocials>
+                    <ContainerSocialsCircleRow>
+                        <ContainercircleSocial href="https://www.instagram.com/churrascariatourao/" target="_blank">
+                            <LogoSocials src={Instagram}/>
+                        </ContainercircleSocial>
+
+                        <ContainercircleSocial href="https://www.facebook.com/ChurrascariaTouraoRio/" target="_blank">
+                            <LogoSocials src={Facebook}/>
+                        </ContainercircleSocial>
+
+                        <ContainercircleSocial href="https://www.tourao.com.br/" target="_blank">
+                            <LogoSocials src={Site}/>
+                        </ContainercircleSocial>
+                    </ContainerSocialsCircleRow>
+                </ContainerSocials>
+                <ContainerCelebration>
+                    <ContainerCelebrationLine>
+                    <TextCelebration>
+                    Faremos uma recepção digna da celebração do nosso amor e contamos com você e sua família e estamos solicitando que confirmem a presença até o dia 07 de junho, uma semana anterior ao evento para garantir uma boa organização e que todos se acomodem bem na área que reservaremos no restaurante. 
+                    </TextCelebration>
+                    </ContainerCelebrationLine>
+                    <ContainerCircleLocation>
+                    <TextLinkLocation href='https://docs.google.com/forms/d/1wbZTYhFdHNXwXtAL362qTu-DKz0NDaMER4xrSgd-cVU/viewform?edit_requested=true' target="_blank">
+                    Clique aqui para confirmar a sua presença
+                    </TextLinkLocation>
+                </ContainerCircleLocation>
+                </ContainerCelebration>
+                <ContainerFooterInvite>
+                    <ContainerFooterInviteTitle>
+                        <TextFooterInvite>
+                        Muitos amigos e familiares gostariam de contribuir com os itens da nossa casa e por isso montamos essa lista de presentes simples com alguns links de referência do modelo, 
+                        cor e tamanho que gostariamos de receber porém fica a seu critério usar esse link ou não. Apenas nos avise para que não haja presentes iguais/duplicado.
+                        </TextFooterInvite>
+                        <ContainerButtonInvite>
+                        <TextLinkLocation href="https://www.notion.so/Lista-de-presentes-948929feeb1349b89acad32f4c17096c" target="_blank">
+                        Clique aqui para conferir
+                        </TextLinkLocation>
+                        </ContainerButtonInvite>
+                    </ContainerFooterInviteTitle>
+                    <ContainerFooterResume>
+                        <TextResumeTitle>
+                            RESUMO:
+                        </TextResumeTitle>
+                        <ContenxtResume>
+                            Churrascaria Tourão - Praça São Perpétuo, 116 - Barra da Tijuca, RJ
+                            Horário do evento: 19h ás 23h (devido horário de funcionamento do estabelecimento)
+                        </ContenxtResume>
+                    </ContainerFooterResume>
+
+                    <Footer>
+                        <TextLastFooter>
+                        ESPERAMOS PODER TER VOCÊ COM A GENTE NESSE MOMENTO TÃO FELIZ DE NOSSAS VIDAS!
+                        </TextLastFooter>
+                    </Footer>
+                </ContainerFooterInvite>
+        </Container>
+    )
+}
